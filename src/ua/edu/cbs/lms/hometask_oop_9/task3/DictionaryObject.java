916b83680349aedef0e65aed95737af3b3aa449d
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-04-13 13:56:27 +0300

    Created DictionaryObject class

--- src/ua/edu/cbs/lms/hometask_oop_9/task3/DictionaryObject.java
+++ src/ua/edu/cbs/lms/hometask_oop_9/task3/DictionaryObject.java
@@ -1,10 +1,10 @@
 package ua.edu.cbs.lms.hometask_oop_9.task3;
 
-public class DictionaryObjekt <TKey, TData>{
+public class DictionaryObject<TKey, TData>{
     private TKey key;
     private TData data;
 
-    public DictionaryObjekt(TKey key, TData data){
+    public DictionaryObject(TKey key, TData data){
         this.key = key;
         this.data = data;
     }
