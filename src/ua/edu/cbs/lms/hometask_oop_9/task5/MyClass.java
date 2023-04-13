6859bfba8e919a4a943cdb301bf351b368293803
Oleksandr UA (AlexesUA) <alexeszab@gmail.com>
2023-04-13 16:01:57 +0300

    Created MyClass class

---
+++ src/ua/edu/cbs/lms/hometask_oop_9/task5/MyClass.java
@@ -0,0 +1,21 @@
+package ua.edu.cbs.lms.hometask_oop_9.task5;
+
+import ua.edu.cbs.lms.hometask_oop_9.task2.ErrorsHandlig;
+
+public class MyClass <T>{
+    T variables;
+
+    private MyClass (T variables){
+        this.variables = variables;
+    }
+
+    public static <T> MyClass<T> createMyClass(T variables){
+        try{
+            return new MyClass<T>(variables);
+        }catch (Exception error){
+            ErrorsHandlig.errorHandling(error);
+            return null;
+        }
+    }
+
+}
